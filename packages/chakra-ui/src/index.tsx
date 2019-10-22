93e045d96011588863eabe7a525b50c50fc48048
rockdev55 <steve264225@gmail.com>
2019-10-22 12:19:36 +0100

    chore: resolve type issues in Accordion

---
+++ packages/chakra-ui/src/index.tsx
@@ -0,0 +1,7 @@
+export * from "./Accordion";
+export * from "./Box";
+export * from "./Collapse";
+export * from "./ColorModeProvider";
+export * from "./CSSReset";
+export * from "./Icon";
+export * from "./Image";
