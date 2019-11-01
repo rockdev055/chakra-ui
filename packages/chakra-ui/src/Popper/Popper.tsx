a991ed365383c14aef697d6341e45002086901aa
rockdev55 <steve264225@gmail.com>
2019-11-01 08:28:48 -0300

    fix: set aria-checked to mixed when partially checked

--- packages/chakra-ui/src/Popper/Popper.tsx
+++
@@ -1,9 +0,0 @@
-import * as React from "react";
-import { Box } from "../Box";
-import { usePopper } from "@chakra-ui/hooks";
-
-function Popper(props: { children: React.ReactNode }): null {
-  return null;
-}
-
-export default Popper;
