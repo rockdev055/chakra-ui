029a31ea7d6502dabe79885135d2a26d27c372a5
rockdev55 <steve264225@gmail.com>
2019-11-05 18:38:43 +0800

    Collapse borders in ButtonGroup with isAttached enabled

--- packages/chakra-ui-layout/src/index.tsx
+++
@@ -1,7 +0,0 @@
-export * from "./Box";
-export * from "./Css";
-export * from "./Flex";
-export * from "./Heading";
-export * from "./Text";
-export * from "./Stack";
-export * from "./VisuallyHidden";
