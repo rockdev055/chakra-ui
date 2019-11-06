f696426daa8f3d6db0f8d8adeabcc5bdad7291df
rockdev55 <steve264225@gmail.com>
2019-11-06 10:56:47 +0100

    Update README.md

--- packages/chakra-ui-hooks/src/usePopper.tsx
+++
@@ -1,2 +0,0 @@
-import usePopper from "use-popper";
-export default usePopper;
--- packages/chakra-ui/src/Popper/Popper.tsx
+++
@@ -1,9 +0,0 @@
-import * as React from "react";
-import { Box } from "@chakra-ui/layout";
-import { usePopper } from "@chakra-ui/hooks";
-
-function Popper(props: { children: React.ReactNode }): null {
-  return null;
-}
-
-export default Popper;
