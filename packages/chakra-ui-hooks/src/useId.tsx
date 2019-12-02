a6b9638f5c68a4c1d3c3740a74f82f7b27b36206
rockdev55 <steve264225@gmail.com>
2019-12-02 23:41:42 +0100

    fix: resolve latest issues and fix types

--- packages/chakra-ui-hooks/src/useId.tsx
+++
@@ -1,9 +0,0 @@
-import { useUID } from "react-uid";
-
-function useId(prefix?: string, idProp?: string) {
-  const uuid = useUID();
-  const id = idProp || uuid;
-  return prefix ? `${prefix}-${id}` : id;
-}
-
-export default useId;
