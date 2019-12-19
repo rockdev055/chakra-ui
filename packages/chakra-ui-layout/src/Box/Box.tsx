32d9b5be228b5b771c34793e18ea7974bca94544
rockdev55 <steve264225@gmail.com>
2019-12-20 03:53:16 +0800

    fix: use `UseNumberInputProps` instead of `useNumberInputProps`

--- packages/chakra-ui-layout/src/Box/Box.tsx
+++
@@ -1,8 +0,0 @@
-import * as React from "react";
-import { chakra, PropsOf } from "@chakra-ui/system";
-
-const Box = chakra.div;
-
-export type BoxProps = PropsOf<typeof Box>;
-
-export default Box;
