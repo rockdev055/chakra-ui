b7e050b392741f6ec869b6be1897765d43794605
rockdev55 <steve264225@gmail.com>
2019-12-22 16:12:21 +0100

    Stack - add 'key' prop when 'shouldWrapChildren' is true

--- packages/chakra-ui-system/src/index.tsx
+++
@@ -1,7 +0,0 @@
-export * from "./chakra";
-export * from "./createChakra";
-export * from "./system";
-export * from "./forward-ref";
-export * from "./create-theme-context";
-export { default as createThemeContext } from "./create-theme-context";
-export { ResponsiveValue, get } from "styled-system";
