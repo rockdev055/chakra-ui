bf1f1feb1e33383478f4e8ea8edce5adaba9cc43
rockdev55 <steve264225@gmail.com>
2020-01-02 11:06:53 +0100

    Update select.mdx

--- packages/layout/src/Box/Box.tsx
+++
@@ -1,7 +0,0 @@
-import { createChakra, PropsOf } from "@chakra-ui/system";
-
-const Box = createChakra("div");
-
-export type BoxProps = PropsOf<typeof Box>;
-
-export default Box;
--- packages/system/src/css.tsx
+++
@@ -1,6 +0,0 @@
-import _css from "@styled-system/css";
-import transformProps from "./system/transform-custom-props";
-import { replacePseudo } from "./system/jsx";
-
-export const css = (styles: object): any =>
-  _css(transformProps(replacePseudo(styles)));
