cbea79b409574b04197416ea47e72625b23cb573
rockdev55 <steve264225@gmail.com>
2020-01-10 22:32:56 +0700

    add next.js example

--- packages/core/src/index.tsx
+++
@@ -1,7 +0,0 @@
-export * from "./Accordion";
-export * from "./Breadcrumb";
-export * from "./Alert";
-export * from "./Avatar";
-export * from "./Badge";
-export * from "./Button";
-export * from "./Code";
