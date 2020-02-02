472f2b11debf3eba8e72de2fec5468d281a72810
rockdev55 <steve264225@gmail.com>
2020-02-02 12:12:41 -0700

    fix(button): default CloseButton to type of button

--- packages/system/src/system/index.tsx
+++
@@ -1,9 +0,0 @@
-export {
-  default as isPropValid,
-  ValidHTMLProps,
-  validHTMLProps,
-} from "./is-prop-valid";
-export { default as system, truncate } from "./system-props";
-export { default as pseudo, selectors } from "./pseudo/pseudo";
-export { SystemProps } from "./system-props-interface";
-export { default as jsx } from "./jsx";
