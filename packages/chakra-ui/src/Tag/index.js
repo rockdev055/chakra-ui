a4c2136aa37d6cd6917681811871ea0ddf4550ab
rockdev55 <steve264225@gmail.com>
2020-02-05 23:21:15 +0100

    Fixed default value of variant, size and variantColor in the Tag page documentation

--- packages/chakra-ui/src/Tag/index.js
+++ packages/chakra-ui/src/Tag/index.js
@@ -26,7 +26,7 @@ const tagSizes = {
   },
 };
 
-export const TagCloseButton = ({ isDisabled, ...props }) => (
+export const TagCloseButton = props => (
   <PseudoBox
     as="button"
     display="flex"
@@ -38,12 +38,6 @@ export const TagCloseButton = ({ isDisabled, ...props }) => (
     outline="none"
     mr={-1}
     opacity="0.5"
-    disabled={isDisabled}
-    _disabled={{
-      opacity: "40%",
-      cursor: "not-allowed",
-      boxShadow: "none",
-    }}
     _focus={{
       boxShadow: "outline",
       bg: "rgba(0, 0, 0, 0.14)",
