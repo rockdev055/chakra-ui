9c48625ad319c4dc52242d7cba3f146cdf59878b
rockdev55 <steve264225@gmail.com>
2020-02-09 08:47:00 +0400

    chore: add input group, input element and component style hook

---
+++ packages/preset-base/src/components/Badge.ts
@@ -0,0 +1,54 @@
+import { addOpacity, generateAlphaColors, getColor } from "@chakra-ui/color";
+import { StyleFunctionProps, getModeColor as get } from "./utils";
+
+function getSolidStyle(props: StyleFunctionProps) {
+  const { variantColor: c } = props;
+  const darkBg = addOpacity(`${c}.500`, 0.6);
+  return {
+    bg: get(props, `${c}.500`, darkBg),
+    color: get(props, `white`, `whiteAlpha.800`),
+  };
+}
+
+function getSubtleStyle(props: StyleFunctionProps) {
+  const { variantColor: c } = props;
+  const alphaColors = generateAlphaColors(`${c}.200`);
+  const darkModeBg = alphaColors[300];
+
+  return {
+    bg: get(props, `${c}.200`, darkModeBg),
+    color: get(props, `${c}.800`, `${c}.200`),
+  };
+}
+
+function getOutlineStyle(props: StyleFunctionProps) {
+  const { variantColor: c, theme: t } = props;
+  const darkModeColor = addOpacity(`${c}.200`, 0.8)(t);
+  const lightModeColor = getColor(t, `${c}.500`);
+  const color = get(props, lightModeColor, darkModeColor);
+
+  return {
+    color,
+    boxShadow: `inset 0 0 0px 1px ` + color,
+  };
+}
+
+const Badge = {
+  baseStyle: {
+    display: "inline-block",
+    paddingX: 1,
+    textTransform: "uppercase",
+    fontSize: "xs",
+    borderRadius: "sm",
+    fontWeight: "bold",
+    whiteSpace: "nowrap",
+    verticalAlign: "middle",
+  },
+  variant: {
+    solid: getSolidStyle,
+    outline: getOutlineStyle,
+    subtle: getSubtleStyle,
+  },
+};
+
+export default Badge;
