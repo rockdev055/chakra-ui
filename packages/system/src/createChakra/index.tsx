fc8d8d8acf27650c960dec8f41cf386f7e1bd85e
rockdev55 <steve264225@gmail.com>
2020-02-11 14:46:20 +0100

    fix: broken link to theme page on badge page

--- packages/system/src/createChakra/index.tsx
+++
@@ -1,3 +0,0 @@
-export { default as createChakra } from "./create-chakra";
-export { CreateChakraOptions, CreateChakraComponent } from "./types";
-export { useComponentStyle } from "./use-component-style";
