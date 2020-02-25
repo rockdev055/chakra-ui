c38b1efc8da4a07ecd036a110edeb0751c7e10b0
rockdev55 <steve264225@gmail.com>
2020-02-25 17:26:20 +0100

    fix: typing for dialog base style

---
+++ packages/Accordion/src/tests/Accordion.test.tsx
@@ -0,0 +1,211 @@
+import React from "react"
+import { userEvent, render, fireEvent } from "@chakra-ui/test-utils"
+import {
+  BaseAccordion,
+  BaseAccordionButton,
+  BaseAccordionItem,
+  BaseAccordionPanel,
+} from "../Accordion.base"
+
+test("Button renders correctly", () => {
+  const { asFragment } = render(
+    <BaseAccordion>
+      <BaseAccordionItem>
+        <BaseAccordionButton>Section 1 title</BaseAccordionButton>
+        <BaseAccordionPanel>Panel 1</BaseAccordionPanel>
+      </BaseAccordionItem>
+    </BaseAccordion>,
+  )
+  expect(asFragment()).toMatchSnapshot()
+})
+
+test("uncontrolled: It opens the accordion panel", () => {
+  const { getByTestId } = render(
+    <BaseAccordion defaultIndex={0}>
+      <BaseAccordionItem>
+        <BaseAccordionButton data-testid="button">
+          Section 1 title
+        </BaseAccordionButton>
+        <BaseAccordionPanel data-testid="panel">Panel 1</BaseAccordionPanel>
+      </BaseAccordionItem>
+    </BaseAccordion>,
+  )
+
+  const button = getByTestId("button")
+  expect(button).toHaveAttribute("aria-expanded", "true")
+})
+
+test("uncontrolled: toggles the accordion on click", () => {
+  const { getByText } = render(
+    <BaseAccordion>
+      <BaseAccordionItem>
+        <BaseAccordionButton>Trigger</BaseAccordionButton>
+        <BaseAccordionPanel>Panel</BaseAccordionPanel>
+      </BaseAccordionItem>
+    </BaseAccordion>,
+  )
+
+  const trigger = getByText("Trigger")
+
+  userEvent.click(trigger)
+  expect(trigger).toHaveAttribute("aria-expanded", "true")
+
+  // you can't toggle an accordion without passing `allowToggle`
+  userEvent.click(trigger)
+  expect(trigger).toHaveAttribute("aria-expanded", "true")
+})
+
+// test that arrow up & down moves focus to next/previous accordion
+test("arrow up & down moves focus to next/previous accordion", () => {
+  const { getByText, getByTestId } = render(
+    <BaseAccordion>
+      <BaseAccordionItem>
+        <BaseAccordionButton>Section 1 title</BaseAccordionButton>
+        <BaseAccordionPanel>Panel 1</BaseAccordionPanel>
+      </BaseAccordionItem>
+
+      <BaseAccordionItem>
+        <BaseAccordionButton>Section 2 title</BaseAccordionButton>
+        <BaseAccordionPanel>Panel 2</BaseAccordionPanel>
+      </BaseAccordionItem>
+    </BaseAccordion>,
+  )
+  const accordionItemOne = getByText("Section 1 title")
+  const accordionItemTwo = getByText("Section 2 title")
+
+  fireEvent.keyDown(accordionItemOne, { key: "ArrowDown", keyCode: 40 })
+  expect(accordionItemTwo).toHaveFocus()
+
+  fireEvent.keyDown(accordionItemTwo, { key: "ArrowUp", keyCode: 38 })
+  expect(accordionItemOne).toHaveFocus()
+})
+
+// test that home & end keys moves focus to first/last accordion
+test("home & end keys moves focus to first/last accordion", () => {
+  const { getByText } = render(
+    <BaseAccordion>
+      <BaseAccordionItem>
+        <BaseAccordionButton>First section</BaseAccordionButton>
+        <BaseAccordionPanel>Panel 1</BaseAccordionPanel>
+      </BaseAccordionItem>
+
+      <BaseAccordionItem>
+        <BaseAccordionButton>Second section</BaseAccordionButton>
+        <BaseAccordionPanel>Panel 1</BaseAccordionPanel>
+      </BaseAccordionItem>
+
+      <BaseAccordionItem>
+        <BaseAccordionButton>Last section</BaseAccordionButton>
+        <BaseAccordionPanel>Panel 2</BaseAccordionPanel>
+      </BaseAccordionItem>
+    </BaseAccordion>,
+  )
+  const first = getByText("First section")
+  const last = getByText("Last section")
+
+  fireEvent.keyDown(first, { key: "Home", keyCode: 36 })
+  expect(first).toHaveFocus()
+
+  fireEvent.keyDown(first, { key: "End", keyCode: 35 })
+  expect(last).toHaveFocus()
+})
+// test the only one accordion can be visible + is not togglable
+// test the only one accordion can be visible + is togglable
+// test that multiple accordions can be opened + is togglable
+
+// it has the proper aria attributes
+test("has the proper aria attributes", () => {
+  const { getByText } = render(
+    <BaseAccordion>
+      <BaseAccordionItem>
+        <BaseAccordionButton>Section 1 title</BaseAccordionButton>
+        <BaseAccordionPanel>Panel 1</BaseAccordionPanel>
+      </BaseAccordionItem>
+    </BaseAccordion>,
+  )
+  const button = getByText("Section 1 title")
+  const panel = getByText("Panel 1")
+
+  expect(button).toHaveAttribute("aria-controls")
+  expect(button).toHaveAttribute("aria-expanded")
+  expect(panel).toHaveAttribute("aria-labelledby")
+})
+
+// test that enter and space can toggle the visiblity
+test("enter and space can toggle the visiblity", () => {
+  const { getByTestId, getByText } = render(
+    <BaseAccordion>
+      <BaseAccordionItem data-testid="accordion-item">
+        <BaseAccordionButton>Section 1 title</BaseAccordionButton>
+        <BaseAccordionPanel>Panel 1</BaseAccordionPanel>
+      </BaseAccordionItem>
+    </BaseAccordion>,
+  )
+  const accordionItem = getByTestId("accordion-item")
+  const button = getByText("Section 1 title")
+
+  fireEvent.keyPress(accordionItem, { key: "Enter", keyCode: 13 })
+  expect(button).toHaveAttribute("aria-expanded", "true")
+
+  fireEvent.keyPress(accordionItem, { keyCode: 32 })
+  expect(button).toHaveAttribute("aria-expanded", "false")
+})
+
+// test that tab moves focus to the next focusable element
+test("tab moves focus to the next focusable element", () => {
+  const { getByTestId } = render(
+    <BaseAccordion data-testid="accordion">
+      <BaseAccordionItem data-testid="one">
+        <BaseAccordionButton>First section</BaseAccordionButton>
+        <BaseAccordionPanel>Panel 1</BaseAccordionPanel>
+      </BaseAccordionItem>
+
+      <BaseAccordionItem data-testid="two">
+        <BaseAccordionButton>Second section</BaseAccordionButton>
+        <BaseAccordionPanel>Panel 1</BaseAccordionPanel>
+      </BaseAccordionItem>
+
+      <BaseAccordionItem data-testid="three">
+        <BaseAccordionButton>Last section</BaseAccordionButton>
+        <BaseAccordionPanel>Panel 2</BaseAccordionPanel>
+      </BaseAccordionItem>
+    </BaseAccordion>,
+  )
+  const accordion = getByTestId("accordion")
+  const first = getByTestId("one")
+
+  fireEvent.keyDown(accordion, { key: "Tab", keyCode: 9 })
+  expect(first).toHaveFocus()
+})
+
+// test that aria-contols for button is same as id for panel
+test("aria-contols for button is same as id for panel", () => {
+  const { getByText, getByRole } = render(
+    <BaseAccordion>
+      <BaseAccordionItem>
+        <BaseAccordionButton>Section 1 title</BaseAccordionButton>
+        <BaseAccordionPanel>Panel 1</BaseAccordionPanel>
+      </BaseAccordionItem>
+    </BaseAccordion>,
+  )
+  const button = getByText("Section 1 title")
+  const panel = getByText("Panel 1")
+  expect(button.getAttribute("aria-controls")).toEqual(panel.getAttribute("id"))
+})
+
+// test that aria-expanded is true/false when accordion is open/closed
+// test that panel has role=region and aria-labelledby
+test("panel has role=region and aria-labelledby", () => {
+  const { getByText } = render(
+    <BaseAccordion>
+      <BaseAccordionItem>
+        <BaseAccordionButton>Section 1 title</BaseAccordionButton>
+        <BaseAccordionPanel>Panel 1</BaseAccordionPanel>
+      </BaseAccordionItem>
+    </BaseAccordion>,
+  )
+  const panel = getByText("Panel 1")
+
+  expect(panel).toHaveAttribute("aria-labelledby")
+  expect(panel).toHaveAttribute("role", "region")
+})
---
+++ packages/NumberInput/src/NumberInput.hook.tsx
@@ -0,0 +1,329 @@
+import {
+  ensureFocus,
+  normalizeEventKey,
+  callAllHandlers,
+  mergeRefs,
+} from "@chakra-ui/utils"
+import * as React from "react"
+import { useCounter, CounterHookProps } from "@chakra-ui/counter"
+import { useUpdateEffect, useInterval, useBooleanState } from "@chakra-ui/hooks"
+import {
+  isFloatingPointNumericCharacter,
+  isValidNumericKeyboardEvent,
+} from "./NumberInput.utils"
+
+export interface NumberInputHookProps extends CounterHookProps {
+  /**
+   * If `true`, the input will be focused as you increment
+   * or decrement the value with the stepper
+   *
+   * @default true
+   */
+  focusInputOnChange?: boolean
+  /**
+   * This controls the value update when you blur out of the input.
+   * - If `true` and the value is greater than `max`, the value will be reset to `max`
+   * - Else, the value remains the same.
+   *
+   * @default true
+   */
+  clampValueOnBlur?: boolean
+  /**
+   * This is used to format the value so that screen readers
+   * can speak out a more human-friendly value.
+   *
+   * It is used to set the `aria-valuetext` property of the input
+   */
+  getAriaValueText?: (value: number | string) => string
+  /**
+   * If `true`, the input will be in readonly mode
+   */
+  isReadOnly?: boolean
+  /**
+   * If `true`, the input will have `aria-invalid` set to `true`
+   */
+  isInvalid?: boolean
+  /**
+   * If `true`, the input will be disabled
+   */
+  isDisabled?: boolean
+  /**
+   * Specifies the value extracted from formatter
+   * @default parseFloat
+   *
+   */
+  parse?: (value: string) => number
+  /**
+   * Specifies the format of the value presented
+   */
+  format?: (value: string | number) => string
+  /**
+   * decimal separator
+   */
+  decimalSeparator?: string
+}
+
+export function useNumberInput(props: NumberInputHookProps = {}) {
+  const {
+    focusInputOnChange = true,
+    clampValueOnBlur = true,
+    keepWithinRange = true,
+    min = -Infinity,
+    max = Infinity,
+    step: stepProp = 1,
+    isReadOnly,
+    isDisabled,
+    getAriaValueText,
+    isInvalid,
+    parse,
+    format,
+    decimalSeparator,
+    onChange: onChangeProp,
+    ...htmlProps
+  } = props
+
+  const counter = useCounter(props)
+
+  const [isFocused, setFocused] = useBooleanState(false)
+
+  const inputRef = React.useRef<HTMLInputElement>(null)
+  const isInteractive = !(isReadOnly || isDisabled)
+
+  useUpdateEffect(() => {
+    if (focusInputOnChange && inputRef.current) {
+      ensureFocus(inputRef.current)
+    }
+  }, [counter.value, focusInputOnChange])
+
+  const increment = (step = stepProp) => {
+    if (!isInteractive) return
+    let valueToUse = +counter.value
+    if (isNaN(valueToUse)) {
+      valueToUse = min
+    }
+    const nextValue = counter.clamp(valueToUse + step)
+    counter.update(nextValue)
+  }
+
+  const decrement = (step = stepProp) => {
+    if (!isInteractive) return
+    let valueToUse = +counter.value
+    if (isNaN(valueToUse)) {
+      valueToUse = min
+    }
+    const nextValue = counter.clamp(valueToUse - step)
+    counter.update(nextValue)
+  }
+
+  const spinner = useSpinner(increment, decrement)
+
+  const onChange = React.useCallback(
+    (event: React.ChangeEvent<HTMLInputElement>) => {
+      const { value } = event.target
+      const valueChars = value.split("")
+      const sanitizedValueChars = valueChars.filter(
+        isFloatingPointNumericCharacter,
+      )
+      const sanitizedValue = sanitizedValueChars.join("")
+      counter.update(sanitizedValue)
+    },
+    // eslint-disable-next-line
+    [counter.update],
+  )
+
+  const onKeyDown = (event: React.KeyboardEvent) => {
+    if (!isValidNumericKeyboardEvent(event)) {
+      event.preventDefault()
+    }
+
+    const eventKey = normalizeEventKey(event)
+    const factor = getIncrementFactor(event)
+    const valueStep = factor * stepProp
+
+    switch (eventKey) {
+      case "ArrowUp":
+        event.preventDefault()
+        increment(valueStep)
+        break
+      case "ArrowDown":
+        event.preventDefault()
+        decrement(valueStep)
+        break
+      case "Home":
+        event.preventDefault()
+        counter.update(min)
+        break
+      case "End":
+        event.preventDefault()
+        counter.update(max)
+        break
+      default:
+        break
+    }
+  }
+
+  const getIncrementFactor = (event: React.KeyboardEvent) => {
+    let ratio = 1
+    if (event.metaKey || event.ctrlKey) {
+      ratio = 0.1
+    }
+    if (event.shiftKey) {
+      ratio = 10
+    }
+    return ratio
+  }
+
+  const validateAndClamp = () => {
+    if (counter.value > max) counter.update(max)
+    if (counter.value < min) counter.update(min)
+  }
+
+  const ariaValueText =
+    typeof getAriaValueText === "function"
+      ? getAriaValueText(counter.value)
+      : undefined
+
+  const onBlur = () => {
+    setFocused.off()
+    if (clampValueOnBlur) {
+      validateAndClamp()
+    }
+  }
+
+  type InputProps = {
+    ref?: React.Ref<HTMLInputElement>
+    onFocus?: React.FocusEventHandler<HTMLInputElement>
+    onBlur?: React.FocusEventHandler<HTMLInputElement>
+    onKeyDown?: React.KeyboardEventHandler<HTMLInputElement>
+    onChange?: React.ChangeEventHandler<HTMLInputElement>
+  }
+
+  type ButtonProps = {
+    onMouseDown?: React.MouseEventHandler
+    onMouseUp?: React.MouseEventHandler
+  }
+
+  return {
+    value: counter.value,
+    valueAsNumber: counter.valueAsNumber,
+    isFocused,
+    isDisabled,
+    isReadOnly,
+    getIncrementButtonProps: (props: ButtonProps = {}) => ({
+      ...props,
+      onMouseDown: callAllHandlers(props.onMouseDown, spinner.up),
+      onMouseUp: callAllHandlers(props.onMouseUp, spinner.stop),
+      disabled: counter.isAtMax,
+    }),
+    getDecrementButtonProps: (props: ButtonProps = {}) => ({
+      ...props,
+      onMouseDown: callAllHandlers(props.onMouseDown, spinner.down),
+      onMouseUp: callAllHandlers(props.onMouseUp, spinner.stop),
+      disabled: counter.isAtMin,
+    }),
+    getInputProps: (props: InputProps = {}) => ({
+      ...props,
+      ref: mergeRefs(inputRef, props.ref),
+      value: counter.value,
+      role: "spinbutton",
+      type: "text",
+      pattern: "[0-9]*",
+      "aria-valuemin": min,
+      "aria-valuemax": max,
+      "aria-disabled": isDisabled,
+      "aria-valuenow": counter.valueAsNumber,
+      "aria-invalid": isInvalid || counter.isOutOfRange,
+      "aria-valuetext": ariaValueText,
+      readOnly: isReadOnly,
+      disabled: isDisabled,
+      autoComplete: "off",
+      autoCorrect: "off",
+      onChange: callAllHandlers(props.onChange, onChange),
+      onKeyDown: callAllHandlers(props.onKeyDown, onKeyDown),
+      onFocus: callAllHandlers(props.onFocus, setFocused.on),
+      onBlur: callAllHandlers(props.onBlur, onBlur),
+    }),
+    htmlProps,
+  }
+}
+
+export type NumberInputHookReturn = ReturnType<typeof useNumberInput>
+
+const CONTINUOUS_CHANGE_DELAY = 300
+const CONTINUOUS_CHANGE_INTERVAL = 50
+
+function useSpinner(increment: Function, decrement: Function) {
+  type Action = "increment" | "decrement"
+
+  // To keep incrementing/decrementing on mousedown, we call that `spinning`
+  const [isSpinning, setIsSpinning] = React.useState(false)
+
+  // This state keeps track of the action ("increment" or "decrement")
+  const [action, setAction] = React.useState<Action | null>(null)
+
+  // To increment the value the first time you mousedown, we call that `runOnce`
+  const [runOnce, setRunOnce] = React.useState(true)
+
+  // Store the timeout instance id in a ref, so we can clear the timeout later
+  const timeoutRef = React.useRef<any>(null)
+
+  // Clears the timeout from memory
+  const removeTimeout = () => clearTimeout(timeoutRef.current)
+
+  /**
+   * useInterval hook provides a performant way to
+   * update the state value at specific interval
+   */
+  useInterval(
+    () => {
+      if (action === "increment") increment()
+      if (action === "decrement") decrement()
+    },
+    isSpinning ? CONTINUOUS_CHANGE_INTERVAL : null,
+  )
+
+  // Function to activate the spinning and increment the value
+  const up = React.useCallback(() => {
+    // increment the first fime
+    if (runOnce) increment()
+
+    // after a delay, keep incrementing at interval ("spinning up")
+    timeoutRef.current = setTimeout(() => {
+      setRunOnce(false)
+      setIsSpinning(true)
+      setAction("increment")
+    }, CONTINUOUS_CHANGE_DELAY)
+  }, [increment, runOnce])
+
+  // Function to activate the spinning and increment the value
+  const down = React.useCallback(() => {
+    // decrement the first fime
+    if (runOnce) decrement()
+
+    // after a delay, keep decrementing at interval ("spinning down")
+    timeoutRef.current = setTimeout(() => {
+      setRunOnce(false)
+      setIsSpinning(true)
+      setAction("decrement")
+    }, CONTINUOUS_CHANGE_DELAY)
+  }, [decrement, runOnce])
+
+  // Function to stop spinng (useful for mouseup, keyup handlers)
+  const stop = React.useCallback(() => {
+    setRunOnce(true)
+    setIsSpinning(false)
+    removeTimeout()
+  }, [])
+
+  /**
+   * If the component unmounts while spinning,
+   * let's clear the timeout as well
+   */
+  React.useEffect(() => {
+    return () => {
+      removeTimeout()
+    }
+  }, [])
+
+  return { up, down, stop }
+}
