8dae2207db7f0913b2e093d8547ce824f8be2ac6
rockdev55 <steve264225@gmail.com>
2020-02-25 23:20:59 +0400

    fix: wrap modal content in animated and fix numberinput

--- packages/Editable/README.md
+++
@@ -1,3 +0,0 @@
-## Editable Text
-
-// TODO
--- packages/layout/src/Box.tsx
+++
@@ -1,5 +0,0 @@
-import { createChakra, PropsOf } from "@chakra-ui/system"
-
-export const Box = createChakra("div")
-export type BoxProps = PropsOf<typeof Box>
-export default Box
