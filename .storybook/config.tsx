69672a74c0db768f65773ebd0ca8b1819ed843db
rockdev55 <steve264225@gmail.com>
2020-03-01 11:07:59 +0800

    feat(checkbox): add aria-labelledby attribute to input

--- .storybook/config.tsx
+++
@@ -1,9 +0,0 @@
-import { configure, addDecorator } from "@storybook/react"
-import setup from "./story.setup"
-
-configure(
-  require.context(`../packages/parser/src/`, true, /\.stories\.(js|ts)x?$/),
-  module,
-)
-
-addDecorator(setup)
