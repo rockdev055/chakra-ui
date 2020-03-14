9a8d565943e3f6288f367e36a0cb35f4f51596b4
rockdev55 <steve264225@gmail.com>
2020-03-14 16:26:32 +0400

    chore: updates

--- packages/preset-base/src/components/Spinner.ts
+++
@@ -1,9 +0,0 @@
-export default {
-  variantSize: {
-    xs: { size: "0.75rem" },
-    sm: { size: "1rem" },
-    md: { size: "1.5rem" },
-    lg: { size: "2rem" },
-    xl: { size: "3rem" },
-  },
-}
