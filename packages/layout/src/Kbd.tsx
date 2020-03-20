0b5bc12a2f5cb428f64b5e1076c447f0b302592b
rockdev55 <steve264225@gmail.com>
2020-03-20 20:55:12 +0100

    build: skeleton styles are cached with useMemo

--- packages/layout/src/Kbd.tsx
+++
@@ -1,3 +0,0 @@
-import { createChakra } from "@chakra-ui/system"
-const Kbd = createChakra("kbd", { themeKey: "Kbd" })
-export default Kbd
--- packages/toast/src/index.ts
+++
@@ -1,3 +0,0 @@
-export * from "./Toast.hook"
-export { toast } from "./Toast.class"
-export * from "./Toast.types"
