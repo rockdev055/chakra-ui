0cb9ea056f8e35a72f39793fafa5589b80a16fb0
rockdev55 <steve264225@gmail.com>
2020-05-11 15:03:27 +0100

    chore: add migration file for collapse component

--- packages/collapse/src/Collapse.stories.tsx
+++ packages/collapse/src/Collapse.stories.tsx
@@ -1,5 +1,5 @@
 import * as React from "react"
-import { Collapse } from "./Collapse"
+import { Collapse } from "."
 import { useDisclosure } from "@chakra-ui/hooks"
 
 export default {
@@ -42,4 +42,25 @@ export function WithStringAsChild() {
       </Collapse>
     </>
   )
+}
+
+function setCookie(name: string, value: any, days?: number) {
+  let expires = ""
+  if (days) {
+    const date = new Date()
+    date.setTime(date.getTime() + days * 24 * 60 * 60 * 1000)
+    expires = "; expires=" + date.toUTCString()
+  }
+  document.cookie = name + "=" + (value || "") + expires + "; path=/"
+}
+
+function getCookie(name) {
+  const nameEQ = name + "="
+  const ca = document.cookie.split(";")
+  for (let i = 0; i < ca.length; i++) {
+    let c = ca[i]
+    while (c.charAt(0) == " ") c = c.substring(1, c.length)
+    if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length)
+  }
+  return null
 }
