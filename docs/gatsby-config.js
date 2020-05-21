ad8358d47be544cf9d803f0fb2092c3374d34c6a
rockdev55 <steve264225@gmail.com>
2020-05-20 20:52:45 -0400

    chore(lint-staged): clean up and fix lintstagedrc

--- docs/gatsby-config.js
+++ docs/gatsby-config.js
@@ -36,13 +36,13 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: "Chakra UI",
-        short_name: "Chakra UI",
+        name: "gatsby-default-mdx-basic",
+        short_name: "starter",
         start_url: "/",
-        background_color: "#000000",
-        theme_color: "#319795",
+        background_color: "#663399",
+        theme_color: "#663399",
         display: "minimal-ui",
-        icon: "../logo/logomark-colored@2x.png", // This path is relative to the root of the site.
+        icon: "src/images/gatsby-icon.png", // This path is relative to the root of the site.
       },
     },
     {
