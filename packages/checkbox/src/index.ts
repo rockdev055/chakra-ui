1abaad0784813a1802cbfed6e3e66ae028922f0d
rockdev55 <steve264225@gmail.com>
2020-05-21 05:46:08 +0400

    chore: setup

--- packages/checkbox/src/index.ts
+++
@@ -1,5 +0,0 @@
-export * from "./Checkbox.hook"
-export * from "./Checkbox"
-export * from "./CheckboxGroup.hook"
-export * from "./CheckboxGroup"
-// export * from "./Checkbox.base"
--- packages/color-mode/src/index.ts
+++
@@ -1,2 +0,0 @@
-export * from "./ColorModeProvider"
-export * from "./InitializeColorMode"
