9bd1a884bae12011452556adc95266e11183fb6e
rockdev55 <steve264225@gmail.com>
2020-05-26 20:32:04 +0400

    docs: add component illustrations

---
+++ packages/radio/src/RadioGroup.tsx
@@ -0,0 +1,81 @@
+import { ThemingProps, chakra, PropsOf } from "@chakra-ui/system"
+import { createContext, __DEV__, cx } from "@chakra-ui/utils"
+import * as React from "react"
+import {
+  useRadioGroup,
+  UseRadioGroupProps,
+  UseRadioGroupReturn,
+} from "./RadioGroup.hook"
+
+export type RadioGroupContext = Pick<
+  UseRadioGroupReturn,
+  "onChange" | "value" | "name"
+> &
+  Omit<ThemingProps, "orientation">
+
+const [RadioGroupContextProvider, useRadioGroupContext] = createContext<
+  RadioGroupContext
+>({
+  name: "RadioGroupContext",
+  strict: false,
+})
+
+export { useRadioGroupContext }
+
+export type RadioGroupProps = UseRadioGroupProps &
+  Omit<PropsOf<typeof chakra.div>, "onChange" | "value" | "defaultValue"> &
+  Omit<ThemingProps, "orientation"> & { children: React.ReactNode }
+
+/**
+ * Used for multiple radios which are bound in one group,
+ * and it indicates which option is selected.
+ *
+ * @see Docs https://chakra-ui.com/radio
+ */
+export const RadioGroup = React.forwardRef(
+  (props: RadioGroupProps, ref: React.Ref<any>) => {
+    const {
+      colorScheme,
+      size,
+      variant,
+      children,
+      className,
+      ...radioGroupProps
+    } = props
+
+    const {
+      value,
+      onChange,
+      getRootProps,
+      name,
+      htmlProps: rest,
+    } = useRadioGroup(radioGroupProps)
+
+    const group = React.useMemo(
+      () => ({
+        name,
+        size,
+        onChange,
+        colorScheme,
+        value,
+        variant,
+      }),
+      [size, name, onChange, colorScheme, value, variant],
+    )
+
+    const groupProps = getRootProps({ ref, ...rest })
+    const _className = cx("chakra-radio-group", className)
+
+    return (
+      <RadioGroupContextProvider value={group}>
+        <chakra.div {...groupProps} className={_className}>
+          {children}
+        </chakra.div>
+      </RadioGroupContextProvider>
+    )
+  },
+)
+
+if (__DEV__) {
+  RadioGroup.displayName = "RadioGroup"
+}
