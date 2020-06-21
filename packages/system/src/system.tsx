8103746dd6dfef704ab9e8ba9e2e4c312347d16a
rockdev55 <steve264225@gmail.com>
2020-06-21 23:36:44 +0200

    feat: handle null values in mapResponsive

--- packages/system/src/system.tsx
+++ packages/system/src/system.tsx
@@ -3,7 +3,6 @@ import createStyled from "./create-styled"
 import { As, ChakraComponent, Options } from "./system.types"
 import {
   applyProp,
-  componentProps,
   domElements,
   DOMElements,
   pseudoProps,
@@ -13,7 +12,6 @@ import {
 
 function styled<T extends As, P = {}>(component: T, options?: Options<T, P>) {
   return createStyled<T, P>(component, options)(
-    componentProps(options),
     systemProps,
     pseudoProps,
     applyProp(component),
