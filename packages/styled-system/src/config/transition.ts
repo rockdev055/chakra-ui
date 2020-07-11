cdd21f9093625313487a35c18a49d50c4a1da669
rockdev55 <steve264225@gmail.com>
2020-07-11 22:20:33 +0400

    feat: add transition tokens

---
+++ packages/styled-system/src/config/transition.ts
@@ -0,0 +1,40 @@
+import { Config, createParser, system } from "@styled-system/core"
+import * as CSS from "csstype"
+import { ResponsiveValue } from "../utils"
+
+const config: Config = {
+  transition: true,
+  transitionDuration: true,
+  transitionProperty: {
+    property: "transitionProperty",
+    scale: "transition.property",
+  },
+  transitionTimingFunction: {
+    property: "transitionTimingFunction",
+    scale: "transition.easing",
+  },
+}
+
+export interface TransitionProps {
+  /**
+   * The CSS `transition` property
+   */
+  transition?: ResponsiveValue<CSS.TransitionProperty>
+  /**
+   * The CSS `transition-property` property
+   */
+  transitionProperty?: ResponsiveValue<CSS.TransitionPropertyProperty>
+  /**
+   * The CSS `transition-timing-function` property
+   */
+  transitionTimingFunction?: ResponsiveValue<
+    CSS.TransitionTimingFunctionProperty
+  >
+  /**
+   * The CSS `transition-duration` property
+   */
+  transitionDuration?: ResponsiveValue<string>
+}
+
+export const transition = system(config)
+export const transitionParser = createParser(config)
