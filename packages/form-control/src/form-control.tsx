9ad3fabd2fd77ed89ab95a92a0bfd7f853e00487
rockdev55 <steve264225@gmail.com>
2020-07-21 16:56:46 +0400

    chore: fix drawer transitions

--- packages/form-control/src/form-control.tsx
+++ packages/form-control/src/form-control.tsx
@@ -6,7 +6,7 @@ import {
   PropsOf,
   useStyles,
   StylesProvider,
-  useStyleConfig,
+  useMultiStyleConfig,
   ThemingProps,
   omitThemingProps,
 } from "@chakra-ui/system"
@@ -130,9 +130,7 @@ function useProvider(props: FormControlContext) {
 export type FormControlProps = FormControlContext & PropsOf<typeof chakra.div>
 
 /**
- * FormControl
- *
- * React component that provides context such as
+ * FormControl provides context such as
  * `isInvalid`, `isDisabled`, and `isRequired` to form elements.
  *
  * This is commonly used in form elements such as `input`,
@@ -142,7 +140,7 @@ export const FormControl = forwardRef<FormControlProps>(function FormControl(
   props,
   ref,
 ) {
-  const styles = useStyleConfig("Form", props)
+  const styles = useMultiStyleConfig("Form", props)
   const rest = omitThemingProps(props)
   const { htmlProps, ...context } = useProvider(rest)
 
@@ -170,14 +168,7 @@ if (__DEV__) {
   FormControl.displayName = "FormControl"
 }
 
-const StyledLabel = chakra("label", {
-  baseStyle: {
-    display: "block",
-    textAlign: "left",
-  },
-})
-
-export type FormLabelProps = PropsOf<typeof StyledLabel> & ThemingProps
+export type FormLabelProps = PropsOf<typeof chakra.label> & ThemingProps
 
 /**
  * Used to enhance the usability of form controls.
@@ -191,16 +182,20 @@ export const FormLabel = forwardRef<FormLabelProps>(function FormLabel(
   props,
   ref,
 ) {
-  const styles = useStyleConfig("FormLabel", props)
+  const styles = useMultiStyleConfig("FormLabel", props)
 
   const { className, ...rest } = omitThemingProps(props)
   const ownProps = useFormControlLabel(rest)
 
   return (
-    <StyledLabel
+    <chakra.label
       ref={ref}
       className={cx("chakra-form__label", props.className)}
-      __css={styles.label}
+      __css={{
+        display: "block",
+        textAlign: "left",
+        ...styles.label,
+      }}
       {...ownProps}
     />
   )
