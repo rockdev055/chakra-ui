240c3ee6281862226de8183f5573edff4e211367
rockdev55 <steve264225@gmail.com>
2020-08-06 04:25:11 +0400

    chore: update modal and number input

--- packages/system/src/forward-ref.tsx
+++ packages/system/src/forward-ref.tsx
@@ -28,16 +28,23 @@ export type ComponentWithForwardedRef<
 >
 
 export interface ComponentWithAs<T extends As, P> {
-  <TT extends As>(props: PropsWithAs<TT, P>): JSX.Element
-  (props: PropsWithAs<T, P>): JSX.Element
+  <TT extends As>(props: PropsWithAs<TT, P>): React.ReactElement | null
+  (props: PropsWithAs<T, P>): React.ReactElement | null
   displayName?: string
   propTypes?: React.WeakValidationMap<PropsWithAs<T, P>>
   contextTypes?: React.ValidationMap<any>
   defaultProps?: Partial<PropsWithAs<T, P>>
+  /**
+   * @private
+   */
+  id?: string
 }
 
 export function forwardRef<P, T extends As>(
-  comp: (props: PropsFromAs<T, P>, ref: React.RefObject<any>) => JSX.Element,
+  comp: (
+    props: PropsFromAs<T, P>,
+    ref: React.RefObject<any>,
+  ) => React.ReactElement | null,
 ) {
   return (React.forwardRef(comp as any) as unknown) as ComponentWithAs<T, P>
 }
