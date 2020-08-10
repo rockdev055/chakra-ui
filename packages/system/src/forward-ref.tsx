4dae88881de940d13c64fef57ca3530963ce739d
rockdev55 <steve264225@gmail.com>
2020-08-10 16:27:23 +0000

    chore(deps-dev): bump eslint-plugin-testing-library from 3.4.1 to 3.6.0

--- packages/system/src/forward-ref.tsx
+++ packages/system/src/forward-ref.tsx
@@ -3,21 +3,32 @@
  */
 import * as React from "react"
 
-type As = string | React.ComponentType<any>
+export type AssignableRef<T> =
+  | {
+      bivarianceHack(instance: T | null): void
+    }["bivarianceHack"]
+  | React.MutableRefObject<T | null>
+  | null
+
+type As<P = any> = React.ElementType<P>
 
 export type PropsWithAs<T extends As, P> = P &
-  Omit<ComponentPropsWithRef<T>, "as" | "color" | keyof P> & {
-    as?: T | As
+  Omit<React.ComponentPropsWithRef<T>, "as" | "color" | keyof P> & {
+    as?: T
   }
 
-type ComponentPropsWithRef<T extends As> = T extends React.ComponentClass<
-  infer P
+export type PropsFromAs<T extends As, P> = (PropsWithAs<T, P> & { as: T }) &
+  PropsWithAs<T, P>
+
+export type ComponentWithForwardedRef<
+  T extends React.ElementType,
+  P
+> = React.ForwardRefExoticComponent<
+  P & React.HTMLProps<React.ElementType<T>> & React.ComponentPropsWithRef<T>
 >
-  ? React.PropsWithoutRef<P> & React.RefAttributes<InstanceType<T>> // @ts-expect-error
-  : React.PropsWithRef<React.ComponentProps<T>>
 
 export interface ComponentWithAs<T extends As, P> {
-  <TT extends As = T>(props: PropsWithAs<TT, P>): React.ReactElement | null
+  <TT extends As>(props: PropsWithAs<TT, P>): React.ReactElement | null
   (props: PropsWithAs<T, P>): React.ReactElement | null
   displayName?: string
   propTypes?: React.WeakValidationMap<PropsWithAs<T, P>>
@@ -31,7 +42,7 @@ export interface ComponentWithAs<T extends As, P> {
 
 export function forwardRef<P, T extends As>(
   comp: (
-    props: PropsWithAs<T, Omit<P, "children" | "as">>,
+    props: PropsFromAs<T, Omit<P, "children" | "as">>,
     ref: React.RefObject<any>,
   ) => React.ReactElement | null,
 ) {
