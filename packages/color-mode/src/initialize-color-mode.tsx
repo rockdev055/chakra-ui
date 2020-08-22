a93c5ae8e200e7f2b68900bad45e9d734e0ab330
rockdev55 <steve264225@gmail.com>
2020-08-22 18:33:15 -0400

    docs: fix typo in theming

--- packages/color-mode/src/initialize-color-mode.tsx
+++ packages/color-mode/src/initialize-color-mode.tsx
@@ -1,28 +1,10 @@
 import * as React from "react"
-import { ColorMode } from "./color-mode-provider"
 
-function setColorModeVar(initialValue: ColorMode | "system") {
-  const mql = window.matchMedia("(prefers-color-scheme: dark)")
-  const systemPreference = mql.matches ? "dark" : "light"
-
-  const persistedPreference = localStorage.getItem("chakra-ui-color-mode")
-  const isInStorage = typeof persistedPreference === "string"
-
-  let colorMode
-
-  if (isInStorage) {
-    colorMode = persistedPreference
-  } else {
-    colorMode = initialValue === "system" ? systemPreference : initialValue
-  }
-
-  const root = document.documentElement
-  root.style.setProperty("--chakra-ui-color-mode", colorMode)
-}
-
-interface InitialColorModeProps {
-  initialValue?: ColorMode
-}
+const noFlash = `(function() { try {
+  var mode = localStorage.getItem('chakra-ui-color-mode');
+  if (!mode) return
+  document.body.classList.add('chakra-ui-' + mode);
+} catch (e) {} })();`
 
 /**
  * Script to add to the root of your application to help prevent
@@ -30,8 +12,6 @@ interface InitialColorModeProps {
  *
  * This is particular useful for SSR in Gatsby or Next.js
  */
-export const InitializeColorMode = (props: InitialColorModeProps) => {
-  const { initialValue = "light" } = props
-  const __html = `(${String(setColorModeVar)})(\"${initialValue}\")`
-  return <script dangerouslySetInnerHTML={{ __html }} />
-}
+export const InitializeColorMode = () => (
+  <script dangerouslySetInnerHTML={{ __html: noFlash }} />
+)
