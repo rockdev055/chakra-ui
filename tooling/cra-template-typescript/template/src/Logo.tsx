0e21aa248274a1ad1fc2f21686fb26b2f74c664b
rockdev55 <steve264225@gmail.com>
2020-09-02 00:30:29 +0400

    fix: typings for get-radio-prop getter

--- tooling/cra-template-typescript/template/src/Logo.tsx
+++ tooling/cra-template-typescript/template/src/Logo.tsx
@@ -1,5 +1,5 @@
 import * as React from "react"
-import { chakra, keyframes, ImageProps, forwardRef } from "@chakra-ui/core"
+import { keyframes, ImageProps, Image } from "@chakra-ui/core"
 import logo from "./logo.svg"
 
 const spin = keyframes`
@@ -7,15 +7,14 @@ const spin = keyframes`
   to { transform: rotate(360deg); }
 `
 
-export const Logo = forwardRef<ImageProps, "img">((props, ref) => (
-  <chakra.img
+export const Logo = (props: ImageProps) => (
+  <Image
     css={{
       "@media (prefers-reduced-motion: no-preference)": {
         animation: `${spin} infinite 20s linear`,
       },
     }}
     src={logo}
-    ref={ref}
     {...props}
   />
-))
+)
