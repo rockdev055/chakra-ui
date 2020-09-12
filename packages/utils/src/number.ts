d0abf566f63a74e27ba69a6d5ac478a2cf2b123a
rockdev55 <steve264225@gmail.com>
2020-09-12 12:24:03 +0200

    fix: useStyleConfig omit children when merging

--- packages/utils/src/number.ts
+++ packages/utils/src/number.ts
@@ -92,7 +92,7 @@ export function clampValue(value: number, min: number, max: number) {
 
   warn({
     condition: max < min,
-    message: "clamp: max cannot be less than min",
+    message: "[Chakra UI] clamp: max cannot be less than min",
   })
 
   return Math.min(Math.max(value, min), max)
