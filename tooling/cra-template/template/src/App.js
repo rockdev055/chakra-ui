cda5bf65de0c5740d1116cef9f7d83aba5f565f9
rockdev55 <steve264225@gmail.com>
2020-09-18 18:30:16 +0530

    :memo: migration guide use defaultIsOpen instead of isOpen

--- tooling/cra-template/template/src/App.js
+++ tooling/cra-template/template/src/App.js
@@ -7,14 +7,14 @@ import {
   VStack,
   Code,
   Grid,
-  theme,
 } from '@chakra-ui/core';
+import theme from '@chakra-ui/theme';
 import { ColorModeSwitcher } from './ColorModeSwitcher';
 import { Logo } from './Logo';
 
 function App() {
   return (
-    <ChakraProvider theme={theme}>
+    <ChakraProvider resetCSS theme={theme}>
       <Box textAlign="center" fontSize="xl">
         <Grid minH="100vh" p={3}>
           <ColorModeSwitcher justifySelf="flex-end" />
